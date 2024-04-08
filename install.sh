087a9268bca6a8c6539dcdd363930379c06e910d
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2024-04-09 03:17:54 +0900

    -

--- install.sh
+++ install.sh
@@ -1,69 +1,53 @@
 #!/bin/bash
 
+USE_ZEN_KERNEL=true
+USE_SYSTEMD_BOOT=true
+DESKTOP='hyprland' # 'plasma'
+DISPLAY_ORIENTATION=0 # CHUWI MiniBook X N100 : 3
+TIMEZONE='Asia/Tokyo'
+
+
 CPU=$(grep 'model name' /proc/cpuinfo|grep -Pio -m1 'intel|amd'|awk '{print tolower($0)}')
 GPU=$(lspci|grep -Pio -m1 'intel|amd|nvidia'|awk '{print tolower($0)}')
-BASE_PKGS='
- base base-devel linux-firmware ${CPU}-ucode
- efibootmgr sudo nano git
- bluez bluez-utils chezmoi
- htop btop smartmontools lsplug powertop smartmontools ufw
+
+echo CPU: $CPU
+echo GPU: $GPU
+
+BASE_PKGS="
+ base base-devel $($USE_ZEN_KERNEL && echo linux-zen || echo linux) linux-firmware $CPU-ucode
+ $(
+	[ $GPU == 'intel' ] && echo 'intel-media-driver intel-gpu-tools' ||
+	[ $GPU == 'amd' ] && echo 'libva-mesa-driver' ||
+	[ $GPU == 'nvidia' ] && echo 'nvidia-dkms' ||
+	echo '[ERR] Unknown GPU'>&2
+	)
+ efibootmgr sudo nano git htop bluez bluez-utils
  noto-fonts noto-fonts-cjk noto-fonts-emoji otf-monaspace
  fcitx5-im fcitx5-mozc
- arch-install-scripts
- man-db
-'
-HYPRLAND_PKGS='
+ btop smartmontools lsplug powertop smartmontools ufw
+ arch-install-scripts man-db chezmoi
+"
+HYPRLAND_PKGS="
  hyprland mako pipewire pipewire-pulse xdg-desktop-portal-hyprland xfce-polkit qt5-wayland qt6-wayland
  waybar hyprpaper wofi cliphist grimblast wlsunset wl-mirror brightnessctl
+ hyprlock hypridle hyprpicker wev
  greetd-tuigreet-bin
- thunar gvfs thunar-volman thunar-media-tags-plugin tumbler ffmpegthumbnailer zip wev
+ thunar gvfs thunar-volman thunar-media-tags-plugin tumbler ffmpegthumbnailer zip
  foot
  gnome-keyring
- hyprlock hypridle hyprpicker
- iwgtk pavucontrol nwg-look-bin gnome-themes-extra papirus-icon-theme bibata-cursor-theme-bin qt5ct qt6ct
-'
-PLASMA_PKGS='
- plasma-meta
- dolphin konsole spectacle ufw
-'
+ iwgtk pavucontrol nwg-look-bin qt5ct qt6ct
+ gnome-themes-extra papirus-icon-theme bibata-cursor-theme-bin
+"
+PLASMA_PKGS="plasma-meta dolphin konsole spectacle"
 
-case $yn in
-	'intel' ) BASE_PKGS='${BASE_PKGS} intel-media-driver intel-gpu-tools' ;;
-	'amd' ) BASE_PKGS='${BASE_PKGS} libva-mesa-driver' ;;
-	'nvidia' ) ;;
-esac
 
-while true; do
-	read -n1 -p "rotate display = [yN] " yn;echo
-	case $yn in
-		[Yy]* ) ROTATE=true; break;;
-		[Nn]*|'' ) ROTATE=false; break;;
-	esac
-done
+echo $BASE_PKGS
 
-while true; do
-	read -n1 -p "kernel = [Yn] ? linux-zen : linux " yn;echo
-	case $yn in
-		[Yy]*|'' ) ZEN=true; BASE_PKGS='${BASE_PKGS} linux-zen'; break;;
-		[Nn]* ) ZEN=false; BASE_PKGS='${BASE_PKGS} linux'; break;;
-	esac
-done
+read -p "Are you sure? [yes] " yn
+if [[ $yn != 'yes' ]]; then exit; fi
 
-while true; do
-	read -n1 -p "bootloader = [Yn] ? systemd-boot : grub " yn;echo
-	case $yn in
-		[Yy]*|'' ) SBOOT=true; break;;
-		[Nn]* ) SBOOT=false; BASE_PKGS='${BASE_PKGS} grub os-prober'; break;;
-	esac
-done
-
-while true; do
-	read -n1 -p "desktop = [Yn] ? hyprland : plasma " yn;echo
-	case $yn in
-		[Yy]*|'' ) HYPRLAND=true; BASE_PKGS='${BASE_PKGS} iwd'; YAY_PKGS=$HYPRLAND_PKGS; break;;
-		[Nn]* ) HYPRLAND=false; BASE_PKGS='${BASE_PKGS} networkmanager'; YAY_PKGS=$PLASMA_PKGS break;;
-	esac
-done
+echo ok
+exit
 
 pacman -Sy archlinux-keyring
 pacstrap -K /mnt $BASE_PKGS
@@ -71,7 +55,7 @@ genfstab -U /mnt >> /mnt/etc/fstab
 
 cat <<EOF | arch-chroot /mnt
 
-ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
 
 # bootloader
 

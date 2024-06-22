3955092f9cbf26c9f5027244e52cf4b6dddec2b1
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2024-06-22 19:56:22 +0900

    -

--- install.sh
+++ install.sh
@@ -10,19 +10,19 @@ echo CPU_VENDOR: $CPU_VENDOR
 echo GPU_VENDOR: $GPU_VENDOR
 
 PKGS='
- hyprland mako pipewire pipewire-pulse pipewire-jack xdg-desktop-portal-hyprland xfce-polkit qt5-wayland qt6-wayland
- waybar hyprpaper wofi cliphist grimblast wlsunset wl-mirror brightnessctl
- hyprlock hypridle hyprpicker wev
- greetd greetd-tuigreet-bin
- thunar gvfs thunar-volman thunar-media-tags-plugin tumbler ffmpegthumbnailer zip
- foot
- gnome-keyring
- iwgtk pavucontrol nwg-look-bin qt5ct qt6ct
- gnome-themes-extra papirus-icon-theme bibata-cursor-theme-bin
- noto-fonts noto-fonts-cjk noto-fonts-emoji otf-monaspace
- fcitx5-im fcitx5-mozc
- btop smartmontools lsplug powertop ufw
- arch-install-scripts dosfstools exfatprogs chezmoi
+#  hyprland mako pipewire pipewire-pulse pipewire-jack xdg-desktop-portal-hyprland xfce-polkit qt5-wayland qt6-wayland
+#  waybar hyprpaper wofi cliphist grimblast wlsunset wl-mirror brightnessctl
+#  hyprlock hypridle hyprpicker wev
+#  greetd greetd-tuigreet-bin
+#  thunar gvfs thunar-volman thunar-media-tags-plugin tumbler ffmpegthumbnailer zip
+#  foot
+#  gnome-keyring
+#  iwgtk pavucontrol nwg-look-bin qt5ct qt6ct
+#  gnome-themes-extra papirus-icon-theme bibata-cursor-theme-bin
+#  noto-fonts noto-fonts-cjk noto-fonts-emoji otf-monaspace
+#  fcitx5-im fcitx5-mozc
+#  btop smartmontools lsplug powertop ufw
+#  arch-install-scripts dosfstools exfatprogs chezmoi
 '
 
 pacman -Sy --noconfirm brightnessctl
@@ -32,8 +32,8 @@ pacstrap -K /mnt base linux-zen linux-zen-headers linux-firmware $CPU_VENDOR-uco
 	[ $GPU_VENDOR == 'amd' ] && echo 'libva-mesa-driver' ||
 	[ $GPU_VENDOR == 'nvidia' ] && echo 'nvidia-dkms' ||
 	echo '[ERR] Unknown GPU_VENDOR'>&2
-) efibootmrg sudo nano git man-db base-devel iwd bluez bluez-utils
-genfstab -U /mnt >> /mnt/etc/fstab
+) efibootmgr sudo nano git man-db base-devel iwd bluez bluez-utils
+genfstab -U /mnt |tee /mnt/etc/fstab
 cp /etc/systemd/network/* /mnt/etc/systemd/network
 
 cat <<EOF | arch-chroot /mnt
@@ -54,26 +54,26 @@ sed -i -E 's/# (Defaults env_keep \+= "HOME"|%wheel ALL=\(ALL:ALL\) ALL)/\1/g' /
 
 bootctl install
 
-cat <<_EOF >> /boot/loader/loader.conf
+cat <<_EOF |tee /boot/loader/loader.conf
 default arch-zen.conf
 timeout 3
 console-mode keep
 editor no
 _EOF
 
-cat <<_EOF >> /boot/loader/entries/arch-zen.conf
+cat <<_EOF |tee /boot/loader/entries/arch-zen.conf
 title Arch Linux w/ ZEN Kernel
 linux /vmlinuz-linux-zen
 initrd /$CPU_VENDOR-ucode.img
 initrd /initramfs-linux-zen.img
-options root=$(grep -oP 'UUID=\S+(?=\s+\/\s)' /etc/fstab) rw
+options root=$(grep -oP 'UUID=\S+(?=\s+\/\s)' /mnt/etc/fstab) rw
 options quiet splash
-options resume=$(grep -oP 'UUID=\S+(?=.+?swap)' /etc/fstab)
+options resume=$(grep -oP 'UUID=\S+(?=.+?swap)' /mnt/etc/fstab)
 # options video=DSI-1:panel_orientation=right_side_up
 _EOF
 
 sed -i -E 's/^(HOOKS=\(base)( udev.+?filesystems)( fsck\))/\1 plymouth\2 resume\3/' /etc/mkinitcpio.conf
-pacman -S --nonconfirm plymouth
+pacman -S --noconfirm plymouth
 
 
 sed -i -E 's/#(Color|VerbosePkgLists|ParallelDownloads)/\1/g' /etc/pacman.conf

c3c59f3360c2680c3e71a3608c08b843e067dcaf
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2024-06-29 07:51:34 +0900

    -

--- setup.sh
+++ setup.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 trap 'echo exitting...;exit' INT
-read -p "Are you sure you want to continue? [yes] " ans;[[ $ans != 'yes' ]] && exit
 
 [ $PKGS ] || PKGS='
 	hyprland mako pipewire pipewire-pulse pipewire-jack xdg-desktop-portal-hyprland xfce-polkit qt5-wayland qt6-wayland \
 	waybar hyprpaper wofi cliphist grimblast wlsunset wl-mirror brightnessctl \
-	hyprlock hypridle hyprpicker wev \
+	hyprlock hypridle hyprpicker wev evtest\
 	greetd greetd-tuigreet-bin \
-	thunar gvfs thunar-volman thunar-media-tags-plugin tumbler ffmpegthumbnailer zip \
+	thunar gvfs thunar-volman thunar-media-tags-plugin tumbler ffmpegthumbnailer zip unzip p7zip \
 	foot \
 	gnome-keyring \
-	iwgtk pavucontrol nwg-look-bin qt5ct qt6ct \
+	iwgtk pavucontrol nwg-look-bin qt5ct qt6ct wdisplays \
 	gnome-themes-extra papirus-icon-theme bibata-cursor-theme-bin \
 	noto-fonts noto-fonts-cjk noto-fonts-emoji otf-monaspace \
 	fcitx5-im fcitx5-mozc \ 
 	btop smartmontools lsplug powertop \
-	arch-install-scripts dosfstools exfatprogs chezmoi \
-	firefox code gimp mpv
+	arch-install-scripts dosfstools exfatprogs ntfs-3g chezmoi npm \
+	swayfx swayidle swaylock-effects idlehack autotiling-rs \
+	firefox code gimp mpv discord_arch_electron \
+	cava cowsay nyancat figlet cmatrix neofetch pipes.sh sl
 '
-#!/bin/bash
-which ya && {
+which yay || {
 	echo Installing yay...
 	git clone --depth=1 https://aur.archlinux.org/yay-bin
 	cd yay-bin
@@ -36,5 +36,3 @@ chezmoi status || {
 	sudo cp -r usr etc /
 	chezmoi apply
 }
-
-cd ~

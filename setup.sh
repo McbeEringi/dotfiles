eb1bc1567b70163950206b609ac4d437b322855e
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2024-06-29 08:11:40 +0900

    -

--- setup.sh
+++ setup.sh
@@ -2,21 +2,21 @@
 trap 'echo;echo exitting...;exit' INT
 
 [ $PKGS ] || PKGS='
-	hyprland mako pipewire pipewire-pulse pipewire-jack xdg-desktop-portal-hyprland xfce-polkit qt5-wayland qt6-wayland \
-	waybar hyprpaper wofi cliphist grimblast wlsunset wl-mirror brightnessctl \
+	hyprland mako pipewire pipewire-pulse pipewire-jack xdg-desktop-portal-hyprland xfce-polkit qt5-wayland qt6-wayland
+	waybar hyprpaper wofi cliphist grimblast wlsunset wl-mirror brightnessctl
 	hyprlock hypridle hyprpicker wev evtest\
-	greetd greetd-tuigreet-bin \
-	thunar gvfs thunar-volman thunar-media-tags-plugin tumbler ffmpegthumbnailer zip unzip p7zip \
-	foot \
-	gnome-keyring \
-	iwgtk pavucontrol nwg-look-bin qt5ct qt6ct wdisplays \
-	gnome-themes-extra papirus-icon-theme bibata-cursor-theme-bin \
-	noto-fonts noto-fonts-cjk noto-fonts-emoji otf-monaspace \
-	fcitx5-im fcitx5-mozc \ 
-	btop smartmontools lsplug powertop \
-	arch-install-scripts dosfstools exfatprogs ntfs-3g chezmoi npm \
-	swayfx swayidle swaylock-effects idlehack autotiling-rs \
-	firefox code gimp mpv discord_arch_electron \
+	greetd greetd-tuigreet-bin
+	thunar gvfs thunar-volman thunar-media-tags-plugin tumbler ffmpegthumbnailer zip unzip p7zip
+	foot
+	gnome-keyring
+	iwgtk pavucontrol nwg-look-bin qt5ct qt6ct wdisplays
+	gnome-themes-extra papirus-icon-theme bibata-cursor-theme-bin
+	noto-fonts noto-fonts-cjk noto-fonts-emoji otf-monaspace
+	fcitx5-im fcitx5-mozc 
+	btop smartmontools lsplug powertop
+	arch-install-scripts dosfstools exfatprogs ntfs-3g chezmoi npm
+	swayfx swayidle swaylock-effects idlehack autotiling-rs
+	firefox code gimp mpv discord_arch_electron
 	cava cowsay nyancat figlet cmatrix neofetch pipes.sh sl
 '
 which yay || {

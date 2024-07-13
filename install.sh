c70af8883837c546ccbd0b990de98b2f392ba29f
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2024-07-14 00:21:18 +0900

    -

--- install.sh
+++ install.sh
@@ -19,8 +19,7 @@ PACMAN_CONF_MODIFY="sed -i -E 's/^#(Color|VerbosePkgLists|ParallelDownloads)/\1/
 
 timedatectl
 eval $PACMAN_CONF_MODIFY
-pacman -Sy --noconfirm brightnessctl
-brightnessctl s 10%
+pacman -Sy --noconfirm brightnessctl;brightnessctl s 10%
 pacstrap -K /mnt base linux-zen linux-zen-headers linux-firmware $(
 	([ $CPU_VENDOR == 'intel' ] && echo 'intel-ucode ') ||
 	([ $CPU_VENDOR == 'amd' ] && echo 'amd-ucode ')
@@ -28,38 +27,24 @@ pacstrap -K /mnt base linux-zen linux-zen-headers linux-firmware $(
 	([ $GPU_VENDOR == 'intel' ] && echo 'intel-media-driver intel-gpu-tools ') ||
 	([ $GPU_VENDOR == 'amd' ] && echo 'libva-mesa-driver ')
 )efibootmgr edk2-shell sudo nano git man-db base-devel iwd bluez bluez-utils
+cp /etc/systemd/network/* /mnt/etc/systemd/network
 genfstab -U /mnt |tee /mnt/etc/fstab
 ROOT_UUID=$(grep -oP 'UUID=\S+(?=\s+\/\s)' /mnt/etc/fstab)
 SWAP_UUID=$(grep -oP 'UUID=\S+(?=.+?swap)' /mnt/etc/fstab)
-cp /etc/systemd/network/* /mnt/etc/systemd/network
 
-cat <<EOF | arch-chroot /mnt
+MAKEPKG_CONF_MODIFY="sed -i -E -e's/^(COMPRESSZST=\(zstd -c -T0).*?( -\))/\1\2/' -e's/^#(MAKEFLAGS=.*)/\1/' /etc/makepkg.conf"
+MKINITCPIO_CONF_MODIFY="sed -i -E 's/^(HOOKS=\(base)( udev.+?filesystems)( fsck\))/\1 plymouth\2$([[ $SWAP_UUID ]] && echo ' resume')\3/' /etc/mkinitcpio.conf"
+LOCALE_GEN_MODIFY="sed -i -E 's/#($LOCALE_GEN)/\1/g' /etc/locale.gen"
+SUDOERS_MODIFY="sed -i -E 's/# (Defaults env_keep \+= "HOME"|%wheel ALL=\(ALL:ALL\) ALL)/\1/g' /etc/sudoers"
 
-ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
-hwclock --systohc
-sed -i -E 's/#($LOCALE_GEN)/\1/g' /etc/locale.gen
-locale-gen
-echo LANG=$LOCALE_USE|tee /etc/locale.conf # localectl set-locale $LOCALE_USE
-echo KEYMAP=$KEYMAP|tee /etc/vconsole.conf # localectl set-keymap $KEYMAP
-$PACMAN_CONF_MODIFY
-sed -i -E 's/^(COMPRESSZST=\(zstd -c -T0).*?( -\))/\1\2/' /etc/makepkg.conf
-sed -i -E 's/^#(MAKEFLAGS=.*)/\1/' /etc/makepkg.conf
-ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
-sed -i -E 's/^(HOOKS=\(base)( udev.+?filesystems)( fsck\))/\1 plymouth\2$([[ $SWAP_UUID ]] && echo ' resume')\3/' /etc/mkinitcpio.conf
-pacman -S --noconfirm plymouth
-
-
-cp /usr/share/edk2-shell/x64/Shell_Full.efi /boot/shellx64.efi
-bootctl install
-
-cat <<_EOF |tee /boot/loader/loader.conf
+BOOTCTL_LOADER_CONF="cat <<_EOF |tee /boot/loader/loader.conf
 default arch-zen.conf
 timeout 3
 console-mode keep
 editor no
 _EOF
-
-cat <<_EOF |tee /boot/loader/entries/arch-zen.conf
+"
+BOOTCTL_ENTRIES_ARCH_ZEN_CONF="cat <<_EOF |tee /boot/loader/entries/arch-zen.conf
 title Arch Linux w/ ZEN Kernel
 linux /vmlinuz-linux-zen
 $([[ $CPU_VENDOR ]] && echo initrd /$CPU_VENDOR-ucode.img)
@@ -69,15 +54,34 @@ options quiet splash
 $([[ $SWAP_UUID ]] || echo '# ')options resume=$SWAP_UUID
 # options video=DSI-1:panel_orientation=right_side_up
 _EOF
+"
 
+SYSTEMCTL_EN="systemctl enable systemd-networkd systemd-resolved iwd systemd-timesyncd bluetooth"
+
+cat <<EOF | arch-chroot /mnt
+ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+hwclock --systohc
+$LOCALE_GEN_MODIFY
+locale-gen
+echo LANG=$LOCALE_USE|tee /etc/locale.conf # localectl set-locale $LOCALE_USE
+echo KEYMAP=$KEYMAP|tee /etc/vconsole.conf # localectl set-keymap $KEYMAP
+$PACMAN_CONF_MODIFY
+$MAKEPKG_CONF_MODIFY
+ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
+$MKINITCPIO_CONF_MODIFY
+pacman -S --noconfirm plymouth
+
+cp /usr/share/edk2-shell/x64/Shell_Full.efi /boot/shellx64.efi
+bootctl install
+$BOOTCTL_LOADER_CONF
+$BOOTCTL_ENTRIES_ARCH_ZEN_CONF
 
 echo $ROOT_PASS|passwd -s root
 useradd -m -g wheel $USER_NAME
 echo $USER_PASS|passwd -s $USER_NAME
-sed -i -E 's/# (Defaults env_keep \+= "HOME"|%wheel ALL=\(ALL:ALL\) ALL)/\1/g' /etc/sudoers
+$SUDOERS_MODIFY
 
-
-systemctl enable systemd-networkd systemd-resolved iwd systemd-timesyncd bluetooth
+$SYSTEMCTL_EN
 EOF
 
 read -p "Press Enter to reboot..." ans;reboot

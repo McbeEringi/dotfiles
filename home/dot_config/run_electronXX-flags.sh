4de9b68265fab27fc9db122e4bebd3e4fd16ca67
McbeEringi <git@mcbeeringi.dev>
2025-06-13 04:45:07 +0900

    -

--- home/dot_config/run_electronXX-flags.sh
+++ home/dot_config/run_electronXX-flags.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-which pacman 2>/dev/null && {
-	rm $(ls|grep -P 'electron\d+')
+which pacman >/dev/null && {
+	rm $(ls|grep -P 'electron\d+') 2>/dev/null
 	W=($(pacman -Qsq electron|grep -oP 'electron\d+'))
 	for((i=0;i<${#W[@]};i++))do(
 		ln -sf electron-flags.conf ${W[$i]}-flags.conf

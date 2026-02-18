c4875de88995c74cd07514085b041f7794d5a788
McbeEringi <git@mcbeeringi.dev>
2026-02-19 02:00:36 +0900

    -

--- build.mjs
+++ build.mjs
@@ -1,9 +1,9 @@
 #!/bin/bun
 
 await Bun.write(
-	'../index.html',
+	'index.html',
 	await new HTMLRewriter().on('div.md',{element:async e=>e.append(
-		Bun.markdown.html(await Bun.file('../README.md').text()),
+		Bun.markdown.html(await Bun.file('README.md').text()),
 		{html:1}
 	)}).transform(await Bun.file('index.tmpl.html').text())
 );

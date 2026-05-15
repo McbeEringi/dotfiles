51d9cd01a76471a8353ca41b846dec50cad5c9fa
McbeEringi <git@mcbeeringi.dev>
2026-05-16 01:29:53 +0900

    Update setup_.sh

--- setup_.sh
+++ setup_.sh
@@ -2,14 +2,22 @@
 
 yay -S \
 neovim \
+ly kmscon \
 hyprland foot fuzzel waybar \
-wireplumber pavucontrol ly \
+wireplumber pavucontrol \
 monaspace noto-fonts noto-fonts-emoji noto-fonts-cjk \
 pipewire-jack firefox
 
+cat <<_EOF | sudo tee /etc/systemd/system/ly@.service.d/10-restart.conf
+[Service]
+Restart=always
+StartLimitInterval=10s
+StartLimitBurst=100
+_EOF
+
 # for tty1
 sudo systemctl disable getty@tty1 # $(systemctl show "*@tty1*" --state=loaded -P Id)
-sudo systemctl enable ly@tty1
+sudo systemctl enable ly@tty1 # ly-kmsconvt@tty1
 
 # for tty2~6 (autovt)
-# sudo ln -s /usr/lib/systemd/system/ly@.service /etc/systemd/system/autovt@tty2.service
+sudo ln -s /usr/lib/systemd/system/kmsconvt@.service /etc/systemd/system/autovt@tty2.service

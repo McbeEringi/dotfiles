541c19984fdd6f8f05a1202f2fb818a1f300bb74
McbeEringi <git@mcbeeringi.dev>
2026-05-15 20:37:06 +0900

    Update setup_.sh

--- setup_.sh
+++ setup_.sh
@@ -7,5 +7,9 @@ wireplumber pavucontrol ly \
 monaspace noto-fonts noto-fonts-emoji noto-fonts-cjk \
 pipewire-jack firefox
 
-# sudo systemctl disable $(systemctl show "*@tty1*" --state=loaded -P Id)
-# sudo systemctl enable ly@tty1
+# for tty1
+sudo systemctl disable getty@tty1 # $(systemctl show "*@tty1*" --state=loaded -P Id)
+sudo systemctl enable ly@tty1
+
+# for tty2~6 (autovt)
+sudo ln -s /usr/lib/systemd/system/ly@.service /etc/systemd/system/autovt@tty2.service

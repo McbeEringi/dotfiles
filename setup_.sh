38846c5d87b530c8ac577b9fa9b30ef8f3c8ac5b
McbeEringi <git@mcbeeringi.dev>
2026-05-16 03:38:22 +0900

    Update setup_.sh

--- setup_.sh
+++ setup_.sh
@@ -2,7 +2,7 @@
 
 yay -S \
 neovim \
-ly kmscon \
+ly bcon \
 hyprland foot fuzzel waybar \
 wireplumber pavucontrol \
 otf-monaspace noto-fonts noto-fonts-emoji noto-fonts-cjk \
@@ -39,15 +39,10 @@ StartLimitInterval=10s
 StartLimitBurst=100
 _EOF
 
-cat <<_EOF | sudo tee /etc/kmscon/kmscon.conf
-xkb-model=jp106
-xkb-layout=jp
-font-name=Monaspace Argon, Noto Sans CJK JP
-_EOF
-
 # for tty1
 sudo systemctl disable getty@tty1 # $(systemctl show "*@tty1*" --state=loaded -P Id)
 sudo systemctl enable ly@tty1 # ly-kmsconvt@tty1
 
 # for tty2~6 (autovt)
-sudo ln -s /usr/lib/systemd/system/kmsconvt@.service /etc/systemd/system/autovt@tty2.service
+sudo ln -s /usr/lib/systemd/system/bcon@.service /etc/systemd/system/autovt@tty2.service
+sudo ln -s /usr/lib/systemd/system/bcon@.service /etc/systemd/system/autovt@tty3.service

c0413380a69725ca200d91df92fff5fd10c9cd1f
McbeEringi <git@mcbeeringi.dev>
2026-05-15 18:13:59 +0900

    Update setup_.sh

--- setup_.sh
+++ setup_.sh
@@ -6,3 +6,6 @@ hyprland foot fuzzel waybar \
 wireplumber pavucontrol ly \
 monaspace noto-fonts noto-fonts-emoji noto-fonts-cjk \
 pipewire-jack firefox
+
+sudo systemctl disable $(systemctl show "*@tty1*" --state=loaded -P Id)
+sudo systemctl enable ly@tty1

f08b88c707d4df6e785cbd2e43a87d2171e67d2f
McbeEringi <git@mcbeeringi.dev>
2026-05-19 17:02:37 +0900

    -

---
+++ dot_config/Thunar/modify_accels.scm
@@ -0,0 +1,14 @@
+#!/bin/bash
+tempfile="$(mktemp)"
+trap 'rm -rf "${tempfile}"' EXIT
+cat > "${tempfile}"
+
+[[ $(grep -oP 'uca-action-open-terminal-here' "${tempfile}") ]]||cat << EOF >> "${tempfile}"
+(gtk_accel_path "<Actions>/ThunarActions/uca-action-open-terminal-here" "F4")
+EOF
+
+[[ $(grep -oP 'uca-action-open-with-mpv' "${tempfile}") ]]||cat << EOF >> "${tempfile}"
+(gtk_accel_path "<Actions>/ThunarActions/uca-action-open-with-mpv" "F6")
+EOF
+
+cat "${tempfile}"

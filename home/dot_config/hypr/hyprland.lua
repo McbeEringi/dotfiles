ff0926bb54c1a1af6e06484113d79cc7f1e46ba6
McbeEringi <git@6ca.me>
2026-07-16 15:58:19 +0900

    -

--- home/dot_config/hypr/hyprland.lua
+++ home/dot_config/hypr/hyprland.lua
@@ -198,6 +198,7 @@ hl.bind("XF86AudioMicMute",     hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_S
 hl.bind("XF86MonBrightnessUp",  hl.dsp.exec_cmd("brightnessctl -e2 set 5%+"),                  { locked = true, repeating = true })
 hl.bind("XF86MonBrightnessDown",hl.dsp.exec_cmd("brightnessctl -e2 set 5%-"),                  { locked = true, repeating = true })
 hl.bind("Print",hl.dsp.exec_cmd("grimblast copy"),{locked=true})
+hl.bind(mod.."Zenkaku_Hankaku",hl.dsp.exec_cmd("grimblast copy"),{locked=true})
 
 hl.bind("XF86AudioNext",hl.dsp.exec_cmd("playerctl next"),{locked=true})
 hl.bind("XF86AudioPause",hl.dsp.exec_cmd("playerctl pause"),{locked=true})

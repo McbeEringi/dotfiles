7de3fba71b44ad505c0ed5789113f25068ccf85b
McbeEringi <git@6ca.me>
2026-09-10 16:31:08 +0900

    -

--- home/dot_config/hypr/hyprland.lua
+++ home/dot_config/hypr/hyprland.lua
@@ -4,13 +4,13 @@ hl.monitor({output="",mode="preferred",position="auto",scale="1"})
 hl.monitor({output="desc:Nreal XREAL 1S",mode="preferred",position="auto-center-up",scale="0.75"})
 
 local terminal="foot"
-local fileManager = "thunar"
-local menu        = "fuzzel"
+local fileManager="thunar"
+local msg="noctalia msg "
+local drun=hl.dsp.exec_cmd(msg.."panel-open launcher")
 
 hl.on("hyprland.start",function()
 	hl.exec_cmd("fcitx5 -d")
 	hl.exec_cmd("noctalia")
-	-- hl.exec_cmd("wlsunset -l35.7 -L139.8")
 end)
 
 hl.env("HYPRCURSOR_THEME","Bibata-Modern-Ice")
@@ -26,6 +26,7 @@ hl.config({
 	},
 	decoration={
 		rounding=8,
+		rounding_power=3,
 		active_opacity=1.,inactive_opacity=.9,
 		dim_special=.5,
 		blur={enabled=true},
@@ -87,7 +88,7 @@ hl.animation({leaf="monitorAdded",enabled=false})
 hl.gesture({fingers=3,direction="horizontal",action="workspace"})
 hl.gesture({fingers=3,direction="vertical",action="fullscreen"})
 hl.gesture({fingers=4,direction="swipe",action="move"})
-hl.gesture({fingers=4,direction="pinchout",action=function()hl.dispatch(hl.dsp.exec_cmd("qs ipc call drun exec"))end})
+hl.gesture({fingers=4,direction="pinchout",action=function()hl.dispatch(drun)end})
 
 hl.window_rule({name="border-xwayland",match={xwayland=true},border_color="rgba(aaaaaa99)"})
 hl.window_rule({name="float",match={title="float"},float=true})
@@ -97,57 +98,57 @@ hl.window_rule({name="mpv-shallow",match={class="mpv-shallow"},pseudo=true,works
 hl.window_rule({name="suppress-maximize-events",match={class=".*"},suppress_event="maximize"})
 hl.window_rule({name="fix-xwayland-drags",match={class="^$",title="^$",xwayland=true,float=true,fullscreen=false,pin=false},no_focus=true})
 hl.window_rule({name="move-hyprland-run",match={class="hyprland-run"},move="20 monitor_h-120",float=true})
-hl.window_rule({name="opacity-qs",match={class="6ca-shell"},opacity=".9 .8 override"})
-hl.window_rule({name="float-qs-menu",match={class="6ca-shell",title="menu-.*"},float=true,pin=true,stay_focused=true,dim_around=true})
-hl.window_rule({name="float-qs-mirror",match={class="6ca-shell",title="mirror"},opacity="1 override"})
-hl.window_rule({name="float-qs-polkit",match={class="6ca-shell",title="polkit"},float=true,pin=true,stay_focused=true,dim_around=true})
+-- hl.window_rule({name="opacity-qs",match={class="6ca-shell"},opacity=".9 .8 override"})
+-- hl.window_rule({name="float-qs-menu",match={class="6ca-shell",title="menu-.*"},float=true,pin=true,stay_focused=true,dim_around=true})
+-- hl.window_rule({name="float-qs-mirror",match={class="6ca-shell",title="mirror"},opacity="1 override"})
+-- hl.window_rule({name="float-qs-polkit",match={class="6ca-shell",title="polkit"},float=true,pin=true,stay_focused=true,dim_around=true})
 
-hl.layer_rule({name="blur-bar",match={namespace="bar"},blur_popups=true,ignore_alpha=.05})
-hl.layer_rule({name="blur-bar-bg",match={namespace="bar-bg"},blur=true,ignore_alpha=.05})
-hl.layer_rule({name="noanim-notify",match={namespace="notify"},no_anim=true,blur=true,ignore_alpha=.05})
+-- hl.layer_rule({name="blur-bar",match={namespace="bar"},blur_popups=true,ignore_alpha=.05})
+-- hl.layer_rule({name="blur-bar-bg",match={namespace="bar-bg"},blur=true,ignore_alpha=.05})
+-- hl.layer_rule({name="noanim-notify",match={namespace="notify"},no_anim=true,blur=true,ignore_alpha=.05})
 
 local mod="SUPER+"
 local shift="SHIFT+"
 local ctrl="CTRL+"
 
-hl.bind(mod.."C",hl.dsp.window.close())
-hl.bind(mod.."Q",hl.dsp.exec_cmd("gnome-terminal"))
-hl.bind(mod.."SPACE",hl.dsp.exec_cmd("gnome-terminal node",{float=true}))
-hl.bind(mod.."W",hl.dsp.exec_cmd("gnome-terminal iwctl"))
-hl.bind(mod.."E",hl.dsp.exec_cmd(fileManager))
-hl.bind(mod..shift.."R",hl.dsp.exec_cmd(menu))
-hl.bind(mod.."R",hl.dsp.exec_cmd("noctalia msg panel-open launcher"))
--- hl.bind(mod.."M",hl.dsp.exec_cmd("hyprctl dispatch 'hl.dsp.exit()'"))
-hl.bind(mod.."M",hl.dsp.exec_cmd("noctalia msg panel-open session"))
-hl.bind(mod.."V",hl.dsp.exec_cmd("noctalia msg panel-open clipboard"))
-hl.bind(mod.."P",hl.dsp.exec_cmd(""))
+hl.bind(mod.."C",    hl.dsp.window.close())
+hl.bind(mod.."Q",    hl.dsp.exec_cmd("gnome-terminal"))
+hl.bind(mod.."Space",hl.dsp.exec_cmd("gnome-terminal node",{float=true}))
+hl.bind(mod.."W",    hl.dsp.exec_cmd("gnome-terminal iwctl"))
+hl.bind(mod.."E",    hl.dsp.exec_cmd(fileManager))
+hl.bind(mod.."R",    drun)
+hl.bind(mod.."M",    hl.dsp.exec_cmd(msg.."panel-open session"))
+hl.bind(mod.."V",    hl.dsp.exec_cmd(msg.."panel-open clipboard"))
+hl.bind(mod.."P",    hl.dsp.exec_cmd(""))
+hl.bind(mod.."Comma",hl.dsp.exec_cmd(msg.."settings-open"))
+hl.bind(mod.."Tab",  hl.dsp.exec_cmd(msg.."window-switcher"))
 
-hl.bind(mod.."escape",function()hl.config({decoration={screen_shader=""},cursor={no_hardware_cursors=2},debug={damage_tracking=2}})end)
-hl.bind(mod.."F1",function()hl.config({decoration={screen_shader="shader/gb.frag"},cursor={no_hardware_cursors=1},debug={damage_tracking=2}})end)
-hl.bind(mod.."F2",function()hl.config({decoration={screen_shader="shader/sunlight.frag"},cursor={no_hardware_cursors=1},debug={damage_tracking=2}})end)
-hl.bind(mod.."F3",function()hl.config({decoration={screen_shader="shader/edge.frag"},cursor={no_hardware_cursors=1},debug={damage_tracking=1}})end)
-hl.bind(mod.."F4",function()hl.config({decoration={screen_shader="shader/edge-inv.frag"},cursor={no_hardware_cursors=1},debug={damage_tracking=1}})end)
+hl.bind(mod.."Escape",function()hl.config({decoration={screen_shader=""},                    cursor={no_hardware_cursors=2},debug={damage_tracking=2}})end)
+hl.bind(mod.."F1",    function()hl.config({decoration={screen_shader="shader/gb.frag"},      cursor={no_hardware_cursors=1},debug={damage_tracking=2}})end)
+hl.bind(mod.."F2",    function()hl.config({decoration={screen_shader="shader/sunlight.frag"},cursor={no_hardware_cursors=1},debug={damage_tracking=2}})end)
+hl.bind(mod.."F3",    function()hl.config({decoration={screen_shader="shader/edge.frag"},    cursor={no_hardware_cursors=1},debug={damage_tracking=1}})end)
+hl.bind(mod.."F4",    function()hl.config({decoration={screen_shader="shader/edge-inv.frag"},cursor={no_hardware_cursors=1},debug={damage_tracking=1}})end)
 
 for _,x in ipairs({"left","right","up","down"}) do
 	hl.bind(mod..x,hl.dsp.focus({direction=x}))
 	hl.bind(mod..shift..x,hl.dsp.window.swap({direction=x}))
 end
-hl.bind(mod.."next",hl.dsp.focus({window="tiled"}))
-hl.bind(mod.."prior",hl.dsp.focus({window="floating"}))
-hl.bind(mod.."J",hl.dsp.layout("togglesplit"))
-hl.bind(mod.."K",hl.dsp.layout("swapsplit"))
-hl.bind(mod.."F",hl.dsp.window.float({action="toggle"}))
-hl.bind(mod..shift.."F",hl.dsp.window.pseudo())
-hl.bind(mod.."G",hl.dsp.group.toggle())
-hl.bind(mod..shift.."G",hl.dsp.window.move({out_of_group=true}))
-hl.bind(mod.."Home",hl.dsp.group.prev())
-hl.bind(mod.."End",hl.dsp.group.next())
+hl.bind(mod.."next",       hl.dsp.focus({window="tiled"}))
+hl.bind(mod.."prior",      hl.dsp.focus({window="floating"}))
+hl.bind(mod.."J",          hl.dsp.layout("togglesplit"))
+hl.bind(mod.."K",          hl.dsp.layout("swapsplit"))
+hl.bind(mod.."F",          hl.dsp.window.float({action="toggle"}))
+hl.bind(mod..shift.."F",   hl.dsp.window.pseudo())
+hl.bind(mod.."G",          hl.dsp.group.toggle())
+hl.bind(mod..shift.."G",   hl.dsp.window.move({out_of_group=true}))
+hl.bind(mod.."Home",       hl.dsp.group.prev())
+hl.bind(mod.."End",        hl.dsp.group.next())
 hl.bind(mod..shift.."Home",hl.dsp.group.move_window({forward=false}))
-hl.bind(mod..shift.."End",hl.dsp.group.move_window({forward=true}))
-hl.bind("F11",hl.dsp.window.fullscreen({action="toggle"}))
+hl.bind(mod..shift.."End", hl.dsp.group.move_window({forward=true}))
+hl.bind("F11",             hl.dsp.window.fullscreen({action="toggle"}))
 
-hl.bind(mod.."mouse:272",hl.dsp.window.drag(),{mouse=true})
-hl.bind(mod.."mouse:273",hl.dsp.window.resize(),{mouse=true})
+hl.bind(mod.."mouse:272",  hl.dsp.window.drag(),  {mouse=true})
+hl.bind(mod.."mouse:273",  hl.dsp.window.resize(),{mouse=true})
 
 local swapws=function(dst)
 	local mv=function(src,dst)
@@ -200,13 +201,19 @@ hl.bind(mod.."mouse_down",hl.dsp.focus({workspace="e+1"}))
 hl.bind(mod.."mouse_up",hl.dsp.focus({workspace="e-1"}))
 
 
-hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"), { locked = true, repeating = true })
-hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"),      { locked = true, repeating = true })
-hl.bind("XF86AudioMute",        hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),     { locked = true, repeating = true })
-hl.bind("XF86AudioMicMute",     hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),   { locked = true, repeating = true })
+-- hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"), { locked = true, repeating = true })
+-- hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"),      { locked = true, repeating = true })
+-- hl.bind("XF86AudioMute",        hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),     { locked = true, repeating = true })
+-- hl.bind("XF86AudioMicMute",     hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),   { locked = true, repeating = true })
+hl.bind("XF86AudioRaiseVolume",hl.dsp.exec_cmd(msg.."volume-up"),  {locked=true,repeating=true})
+hl.bind("XF86AudioLowerVolume",hl.dsp.exec_cmd(msg.."volume-down"),{locked=true,repeating=true})
+hl.bind("XF86AudioMute",       hl.dsp.exec_cmd(msg.."volume-mute"),{locked=true,repeating=true})
+hl.bind("XF86AudioMicMute",    hl.dsp.exec_cmd(msg.."mic-mute"),   {locked=true,repeating=true})
 
-hl.bind("XF86MonBrightnessUp",  hl.dsp.exec_cmd("brightnessctl -e2 set 5%+"),                  { locked = true, repeating = true })
-hl.bind("XF86MonBrightnessDown",hl.dsp.exec_cmd("brightnessctl -e2 set 5%-"),                  { locked = true, repeating = true })
+-- hl.bind("XF86MonBrightnessUp",  hl.dsp.exec_cmd("brightnessctl -e2 set 5%+"),{ locked = true, repeating = true })
+-- hl.bind("XF86MonBrightnessDown",hl.dsp.exec_cmd("brightnessctl -e2 set 5%-"),{ locked = true, repeating = true })
+hl.bind("XF86MonBrightnessUp",  hl.dsp.exec_cmd(msg.."brightness-up"),  {locked=true,repeating=true})
+hl.bind("XF86MonBrightnessDown",hl.dsp.exec_cmd(msg.."brightness-down"),{locked=true,repeating=true})
 
 hl.bind("Print",hl.dsp.exec_cmd("grimblast copy"),{locked=true})
 hl.bind(shift.."Print",hl.dsp.exec_cmd("grimblast copy area"),{locked=true})
@@ -217,8 +224,12 @@ hl.bind(shift..mod.."Zenkaku_hankaku",hl.dsp.exec_cmd("grimblast copy area"),{lo
 hl.bind(ctrl..mod.."Zenkaku_Hankaku",hl.dsp.exec_cmd("grimblast save screen -|satty -f -"),{locked=true})
 hl.bind(ctrl..shift..mod.."Zenkaku_Hankaku",hl.dsp.exec_cmd("grimblast save area -|satty -f -"),{locked=true})
 
-hl.bind("XF86AudioNext",hl.dsp.exec_cmd("playerctl next"),{locked=true})
-hl.bind("XF86AudioPause",hl.dsp.exec_cmd("playerctl pause"),{locked=true})
-hl.bind("XF86AudioPlay",hl.dsp.exec_cmd("playerctl play-pause"),{locked=true})
-hl.bind("XF86AudioPrev",hl.dsp.exec_cmd("playerctl previous"),{locked=true})
+-- hl.bind("XF86AudioNext",hl.dsp.exec_cmd("playerctl next"),      {locked=true})
+-- hl.bind("XF86AudioPause",hl.dsp.exec_cmd("playerctl pause"),    {locked=true})
+-- hl.bind("XF86AudioPlay",hl.dsp.exec_cmd("playerctl play-pause"),{locked=true})
+-- hl.bind("XF86AudioPrev",hl.dsp.exec_cmd("playerctl previous"),  {locked=true})
+hl.bind("XF86AudioNext", hl.dsp.exec_cmd(msg.."media next"),    {locked=true})
+hl.bind("XF86AudioPause",hl.dsp.exec_cmd(msg.."media pause"),   {locked=true})
+hl.bind("XF86AudioPlay", hl.dsp.exec_cmd(msg.."media toggle"),  {locked=true})
+hl.bind("XF86AudioPrev", hl.dsp.exec_cmd(msg.."media previous"),{locked=true})
 
